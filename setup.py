b5fb0f745608b5cbe6a96f4716d6ba4e425871df
Ankur Das <66483907+AnkurDas-krypto@users.noreply.github.com>
2022-12-27 21:03:23 -0500

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = "sensor",
+    version="0.0.1",
+    author = "Ankur Das",
+    author_email = "ankurdas8017@gmail.com",
+    packages=find_packages(),
+    install_requires = [],
+)
+
+
